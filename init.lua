0dab46d51a38c0d09b152a7a60b87e356af25bc2
Steven Dalamaras <sd@stevendalamaras.dev>
2026-02-05 12:55:48 +0000

    feat: add dotfiles

---
+++ init.lua
@@ -0,0 +1,43 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.signcolumn = "yes"
+vim.o.wrap = false
+vim.o.tabstop = 2
+vim.o.swapfile = false
+vim.o.shiftwidth = 2
+vim.g.mapleader = " "
+vim.o.winborder = "rounded"
+
+vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
+vim.keymap.set('n', '<leader>w', ':write<CR>')
+vim.keymap.set('n', '<leader>q', ':quit<CR>')
+
+vim.pack.add({
+  {src = "https://github.com/vague2k/vague.nvim"},
+  {src = "https://github.com/stevearc/oil.nvim"},
+  {src = "https://github.com/echasnovski/mini.pick"},
+ 	{src = "https://github.com/neovim/nvim-lspconfig"}
+})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if client:supports_method('textDocument/completion') then
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+		end
+  end
+})
+
+require "mini.pick".setup()
+require "oil".setup()
+
+vim.keymap.set('n', '<leader>f', ":Pick files<CR>")
+vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
+vim.keymap.set('n', '<leader>e', ":Oil<CR>")
+
+vim.lsp.enable({"lua_ls"})
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+
+vim.cmd("colorscheme vague")
+vim.cmd(":hi statusline guibg=NONE")
+
